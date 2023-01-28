26aec30756798da75b6f0b2ec894a828c95b581e
Hariharan <hariharansriram17@gmail.com>
2023-01-28 16:28:31 +0000

    create docker application

---
+++ back_end/src/app.ts
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.get('/',(req:Request, res:Response) => {
+    // @ts-ignore
+    res.json({test:'umbe'})
+})
+app.listen(5000, () => console.log("Server started on port 5000"));
